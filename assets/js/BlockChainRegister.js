5a5534ce153fca5e110d725c6bba003ce648f38d
nandemotoken <nandemotoken@gmail.com>
2021-02-02 15:00:18 +0000

    update NFT List Page Mockup

--- assets/js/BlockChainRegister.js
+++ assets/js/BlockChainRegister.js
@@ -1,19 +1,4 @@
-let web3tr;
-let torus;
-let useraddress;
 
-window.onload = async function() {
-    if ( window.location.href.split('/').pop() == "CheckTokens.html"){
-        //window.alert("CheckTokensPage");
-        await newTorus();
-        myurl = "https://blockscout.com/poa/xdai/address/" + useraddress[0]+"/tokens" ;
-        window.window.location.href = myurl ;
-    }
-    if ( window.location.href.split('/').pop() == "CheckTokens2.html"){
-        //window.alert("CheckTokensPage");
-        await newTorus();
-    }
-} 
 
 
 async function registration(){
@@ -25,37 +10,3 @@ async function registration(){
 
 
  
-async function newTorus() {
-
-torus = new Torus({
-  buttonPosition: "top-right" // default: bottom-left
-});
-await torus.init({
-  buildEnv: "production", // default: production
-  enableLogging: true, // default: false
-  network: {
-    host: "mainnet", // default: mainnet
-    chainId: 1, // default: 1
-    networkName: "Main Ethereum Network" // default: Main Ethereum Network
-  },
-loginConfig: {
-    'google': {
-      showOnModal: false,
-    },
-    'facebook': {
-      showOnModal: false,
-    },
-    'discord': {
-      showOnModal: false,
-    },
-},
-  showTorusButton: false // default: true
-});
-await torus.login(); // await torus.ethereum.enable()
-web3tr = await new Web3(torus.provider);
-
-useraddress = await web3tr.eth.getAccounts();
-console.log(useraddress[0]);
-
-}
-
---
+++ assets/js/NFTStamps.js
@@ -0,0 +1,17 @@
+//左から0,1,2,3,4,5。0はテスト用
+nftaddressArray = [ "0x47a66daf612f69361852c28c8508e6f3a0ce533d" , "0xcd8a958d91e2aa450db293b7e2b7180c0a5101b1" , "" , "", "", "" ]
+
+
+//画像左から1,2,3,4,5。0はテスト用のサンプルトークン
+async function enableLink( _num ){
+    const web3rpc = new Web3(new Web3.providers.HttpProvider("https://rpc.xdaichain.com/"))
+    mycontract = await new web3rpc.eth.Contract(abi, nftaddressArray[_num]);
+    console.log(useraddress[0]);
+//    let fromblockchain1 = await mycontract.methods.balanceOf(useraddress[0]).call();
+    let fromblockchain1 = await mycontract.methods.tokenOfOwnerByIndex(useraddress[0] , 0).call();
+    console.log(fromblockchain1);
+//    tmp = 2;
+    document.getElementById('image'+_num).style.opacity = "100%";
+    document.getElementById('link'+_num).href = `https://blockscout.com/poa/xdai/tokens/${nftaddressArray[_num]}/instance/${fromblockchain1}/token-transfers`
+}
+
