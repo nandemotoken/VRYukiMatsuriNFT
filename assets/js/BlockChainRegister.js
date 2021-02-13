c61cfcd6e3a24a486e1b50e25f6c72f690ca9bfa
nandemotoken <nandemotoken@gmail.com>
2021-02-13 02:36:32 +0000

    update tokens

--- assets/js/BlockChainRegister.js
+++ assets/js/BlockChainRegister.js
@@ -24,3 +24,37 @@ async function registration3(){
     window.window.location.href = myurl ;
 }
 
+async function registrationRareYuki(){
+//    window.alert("ok")
+    await newTorus();
+    myurl = "https://yukimatsuriaddresscollector.onrender.com/?address=" + useraddress[0] + "RARE";
+    window.window.location.href = myurl ;
+}
+
+async function registrationVL(){
+//    window.alert("ok")
+    await newTorus();
+    myurl = "https://yukimatsuriaddresscollector.onrender.com/?address=" + useraddress[0] + "VLVL";
+    window.window.location.href = myurl ;
+}
+
+async function registrationCHAT(){
+//    window.alert("ok")
+    await newTorus();
+    myurl = "https://yukimatsuriaddresscollector.onrender.com/?address=" + useraddress[0] + "CHAT";
+    window.window.location.href = myurl ;
+}
+
+async function registrationNEOS(){
+//    window.alert("ok")
+    await newTorus();
+    myurl = "https://yukimatsuriaddresscollector.onrender.com/?address=" + useraddress[0] + "NEOS";
+    window.window.location.href = myurl ;
+}
+
+async function registrationCAST(){
+//    window.alert("ok")
+    await newTorus();
+    myurl = "https://yukimatsuriaddresscollector.onrender.com/?address=" + useraddress[0] + "CAST";
+    window.window.location.href = myurl ;
+}
